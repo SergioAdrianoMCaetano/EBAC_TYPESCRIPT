615bc1940d1acc8c8a3678867480c650fa6a807f
Sérgio Adriano <sergioadrianomc@gmail.com>
2024-05-16 16:19:18 -0300

    exercícios da Prática

---
+++ src/pratica.js
@@ -0,0 +1,12 @@
+"use strict";
+//- PRATICA
+// • Utilizando o TypeScript e a digitação escreva duas funções: uma função de multiplicação que deve receber como resultados dois números e retornar a multiplicação deles, e uma função de saudação que deve receber um nome e retornar a concatenação “Olá “ + nome;
+// • No repositório do curso crie uma branch chamada exercicio_ts e armaze o código TS escrito nesta branch, nos envie o link através da plataforma.
+function multiplicar(n1, n2) {
+    return n1 * n2;
+}
+;
+function dizSaudacao(nome) {
+    return () => `Olá, ${nome}`;
+}
+;
